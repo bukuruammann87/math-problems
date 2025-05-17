3f516e3f1cb4b8099c3f494dab6a749f549ef1e7
bukuruammann87 <bukuruammann87@gmail.com>
2025-05-17 06:15:28 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,11 @@
+function sumEvenNumbers(limit) {
+  let sum = 0;
+  for (let i = 1; i <= limit; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumEvenNumbers(10));
